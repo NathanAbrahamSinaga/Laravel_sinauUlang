e65d5047048f693c7aab22d2becdee0193ac2815
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-12-02 23:30:13 +0700

    kedua

--- resources/views/todo/app.blade.php
+++ resources/views/todo/app.blade.php
@@ -40,11 +40,24 @@
             <div class="col-md-8">
              <div class="card mb-3">
                 <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success">{{ session('success') }}</div>
+                    @endif
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <!-- 02. Form input data -->
                     <form id="todo-form" action="" method="post">
+                        @csrf
                         <div class="input-group mb-3">
                             <input type="text" class="form-control" name="task" id="todo-input"
-                                placeholder="Tambah task baru" required>
+                                placeholder="Tambah task baru" required value="{{ old('task') }}">
                             <button class="btn btn-primary" type="submit">
                                 Simpan
                             </button>
@@ -55,9 +68,9 @@
                 <div class="card">
                     <div class="card-body">
                         <!-- 03. Searching -->
-                        <form id="todo-form" action="" method="get">
+                        <form id="todo-form" action="{{ route('todo') }}" method="get">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" name="search" value="" 
+                                <input type="text" class="form-control" name="search" value="{{ request('search') }}" 
                                     placeholder="masukkan kata kunci">
                                 <button class="btn btn-secondary" type="submit">
                                     Cari
@@ -66,43 +79,57 @@
                         </form>
                         
                         <ul class="list-group mb-4" id="todo-list">
+                            @foreach ($data as $item)           
                             <!-- 04. Display Data -->
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <span class="task-text">Coding</span>
+                                <span class="task-text">
+                                    @if ($item->is_done == '1')
+                                        <del>{{ $item->task }}</del>
+                                    @else
+                                        {{ $item->task }}
+                                    @endif
+                                </span>
                                 <input type="text" class="form-control edit-input" style="display: none;"
-                                    value="Coding">
+                                    value="{{ $item->task }}">
                                 <div class="btn-group">
-                                    <button class="btn btn-danger btn-sm delete-btn">✕</button>
+                                    <form action="{{ route('todo.delete', ['id' => $item->id]) }}" method="POST" onsubmit="return confirm('Apakah anda yakin ingin menghapus data ini?')">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger btn-sm delete-btn">✕</button>
+                                    </form>
                                     <button class="btn btn-primary btn-sm edit-btn" data-bs-toggle="collapse"
-                                        data-bs-target="#collapse-1" aria-expanded="false">✎</button>
+                                        data-bs-target="#collapse-{{ $loop->index }}" aria-expanded="false">✎</button>
                                 </div>
                             </li>
                             <!-- 05. Update Data -->
-                            <li class="list-group-item collapse" id="collapse-1">
-                                <form action="" method="POST">
+                            <li class="list-group-item collapse" id="collapse-{{ $loop->index }}">
+                                <form action="{{ route('todo.update', ['id' => $item->id]) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
                                     <div>
                                         <div class="input-group mb-3">
                                             <input type="text" class="form-control" name="task"
-                                                value="Coding">
-                                            <button class="btn btn-outline-primary" type="button">Update</button>
+                                                value="{{ $item->task }}">
+                                            <button class="btn btn-outline-primary" type="submit">Update</button>
                                         </div>
                                     </div>
                                     <div class="d-flex">
                                         <div class="radio px-2">
                                             <label>
-                                                <input type="radio" value="1" name="is_done"> Selesai
+                                                <input type="radio" value="1" name="is_done" {{ $item->is_done == '1'?'checked':'' }}> Selesai
                                             </label>
                                         </div>
                                         <div class="radio">
                                             <label>
-                                                <input type="radio" value="0" name="is_done"> Belum
+                                                <input type="radio" value="0" name="is_done" {{ $item->is_done == '0'?'checked':'' }}> Belum
                                             </label>
                                         </div>
                                     </div>
                                 </form>
                             </li>
+                            @endforeach
                         </ul>
-                        
+                        {{ $data->links() }}
                         
                     </div>
                 </div>
